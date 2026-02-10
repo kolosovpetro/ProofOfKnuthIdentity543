7f31c4d4467b16bac8f38cc41caba33d384476a4
kolosovpetro <kolosovp94@gmail.com>
2026-02-10 22:43:10 +0100

    work in progress

--- mathematica/ProofOfKnuthIdentity543.m
+++ mathematica/ProofOfKnuthIdentity543.m
@@ -1,6 +1,6 @@
 (* ::Package:: *)
 
-BeginPackage["github-latex-template`"]
+BeginPackage["ProofOfKnuthIdentity543`"]
 
 (*BEGIN: Definitions *)
 A::usage="A[n, k] returns the real coefficient A of non-negative integers n, k such that n <= k."
